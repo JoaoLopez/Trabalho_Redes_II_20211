d1c73629f4fb1c6d9cf19ea7e8e6e6887d9f63ae
Matheus Perrut <matheus.perrut2302@gmail.com>
2021-07-28 22:01:04 -0300

    - Criação da estrutura básica de cadastro de usuários

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { MainPageComponent } from './main-page/main-page.component';
-
-
 
 @NgModule({
   declarations: [MainPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    MatTableModule,
+    MatTooltipModule,
+    ReactiveFormsModule
   ],
   exports: [MainPageComponent]
 })
--- frontend/src/app/core/main-page/main-page.component.ts
+++ frontend/src/app/core/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../interfaces/user.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  user: FormGroup;
+  events = 'Eventos';
+
+  displayedColumns: string[] = ['name', 'ip', 'port'];
+  dataSource: User[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.user = this.fb.group({
+      name: ['', Validators.required],
+      ip: ['', Validators.compose([Validators.required, Validators.pattern(new RegExp('(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}', 'g'))])],
+      port: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
 
+  onFormSubmit() {
+    console.log(this.user.value);
+    console.log(this.user.valid);
+  }
 }

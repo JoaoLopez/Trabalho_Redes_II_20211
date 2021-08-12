5026169bac18d1d41ce8cc540069070bda81d8f3
Matheus Perrut <matheus.perrut2302@gmail.com>
2021-08-12 00:46:57 -0300

    - Adição de comentários em client.js

--- backend/src/client.js
+++ backend/src/client.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/*
+Script responsável pela comunicação via socket tcp com a implementação de servidor realizada em servidor.py,
+e pela comunicação com a aplicação de frontend.
+
+Recebe as comunicações do frontend via WebSocket na porta 4444, e se comunica com o backend pela porta 5000.
+*/
+
+/* Importações */
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -7,6 +15,10 @@ const net = require('net');
 const { spawn } = require("child_process");
 const process = require('process');
 
+/* REGIÃO 1 - INÍCIO DO PROCESSO SERVIDOR */
+/* Região responsável por iniciar o processo servidor (arquivo servidor.py) e encerrá-lo quando o script for encerrado */
+
+/* Finaliza o processo servidor.py ao receber o SIGINT para encerrar o script */
 if (process.platform === "win32") {
     var rl = require("readline").createInterface({
         input: process.stdin,
@@ -24,8 +36,10 @@ process.on("SIGINT", function () {
     process.exit();
 });
 
+/* Inicialização do processo servidor.py */
 const pythonServer = spawn("python3", ["servidor.py"]);
 
+/* Listeners responsáveis por escutar os eventos do processo servidor.py e imprimí-los no console */
 pythonServer.stdout.on("data", data => {
     console.log(`servidor.py stdout: ${data}`);
 });
@@ -41,19 +55,25 @@ pythonServer.on('error', (error) => {
 pythonServer.on("close", code => {
     console.log(`servidor.py :child process exited with code ${code}`);
 });
+/* FIM - REGIÃO 1 */
 
+/* REGIÃO 2 - Escuta dos Websockets */
+/* Região responsável por gerenciar as conexões de WebSocket entre o script js e a aplicação de frontend */
+/* Neste conexto, a variável sockets se refere aos WebSockets e não ao socket que se comunica com servidor.py */
 const client = new net.Socket();
 const users = [];
 const sockets = [];
 
+/* Listener das conexões de WebSocket com o frontend, o evento "connection" é disparado sempre que uma nova conexão com o frontend é iniciada */
 io.on("connection", socket => {
-    // mantida uma estrutura que armazena os sockets para enviar as respostas para os sockets corretos
+    /* Mantida uma estrutura que armazena os sockets para enviar as respostas para os sockets corretos */
     const socketFound = sockets.find(sc => sc.id === socket.id);
     if (!socketFound) {
         console.log('Socket salvo');
         sockets.push(socket);
     }
 
+    /* Apaga o websocket da estrutura de sockets quando há uma desconexão com o frontend (caso de o usuário fechar a janela do Browser) */
     socket.on("disconnect", (reason) => {
         let index = sockets.indexOf(socket);
         const disconnectedUser = users.find(u => u.socketId === socket.id);
@@ -73,6 +93,7 @@ io.on("connection", socket => {
         console.log(`Socket ${socket.id} has disconnected`);
     });
 
+    /* Faz uma chamada ao servidor.py via socket tcp para buscar um usuário pelo seu nome */
     socket.on("getUser", userName => {
         connectToServerSocket().then(_ => {
             const message = `Consultar: ${userName}$${socket.id}`;
@@ -82,7 +103,7 @@ io.on("connection", socket => {
         });
     });
 
-
+    /* Faz uma chamada ao servidor.py via socket tcp para criar um novo usuário */
     socket.on("addUser", user => {
         connectToServerSocket().then(_ => {
             const message = `Registrar: ${user.name}, ${user.ip}, ${user.port}, ${socket.id}`;
@@ -92,6 +113,7 @@ io.on("connection", socket => {
         });
     });
 
+    /* Faz uma chamada ao servidor.py via socket tcp para remover um usuário e encerrar sua conexão/sessão */
     socket.on("removeUser", userName => {
         connectToServerSocket().then(_ => {
             const message = `Fechar conexão: ${userName}$${socket.id}`;
@@ -101,11 +123,16 @@ io.on("connection", socket => {
         });
     });
 
+    /* As emissões de evento feitas pelo objeto io funcionam como broadcast,
+    são enviadas à todos os WebSockets, esta no caso sempre é enviada no início de uma conexão
+    para que o frontend conheça todos os usuários cadastrados
+     */
     io.emit("users", users);
 
     console.log(`Socket ${socket.id} has connected`);
 });
 
+/* Inicia conexão com o servidor.py via socket tcp */
 function connectToServerSocket() {
     return new Promise((resolve, reject) => {
         client.connect(5000, '127.0.0.1', function () {
@@ -115,11 +142,12 @@ function connectToServerSocket() {
     });
 }
 
+/* Buscar por um WebSocket armazenado no array sockets pelo seu id */
 function getSocket(socketId) {
     return sockets.find(socket => socket.id === socketId);
 }
 
-// client.destroy(); para matar conexão
+/* Listener das conexões via socket tcp com o servidor.py */
 client.on('data', function (data) {
     const strData = String(data);
 
@@ -170,6 +198,7 @@ client.on('data', function (data) {
     console.log('Received: ' + data);
 });
 
+/* Listener das conexões http com o frontend, porta de entrada do client.js com o frontend */
 http.listen(4444, '0.0.0.0', () => {
     console.log('Listening on port 4444');
 });

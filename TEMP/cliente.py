4b312962ba20c0484cc16d0b253a58f4ed0ccea7
JoaoLopez <joaolopez@id.uff.br>
2021-09-02 17:28:10 -0300

    Renomeando arquivos

---
+++ TEMP/cliente.py
@@ -0,0 +1,159 @@
+"""
+● Qualquer usuário registrado pode ligar para outro usuário registrado,
+permitindo somente conversação entre dois usuários.
+● Qualquer um dos usuários que participam de uma ligação pode encerrar a
+ligação.
+● O estabelecimento da ligação deve ser feito utilizando-se o protocolo
+UDP.
+● Os módulos cliente e servidor devem imprimir na tela todas as mensagens
+enviadas e recebidas (“convite”, “resposta_ao_convite”, “encerrar_ligação”).
+● A aplicação deve ter uma interface gráfica, mesmo sendo simples, seja
+intuitiva e satisfaça as funcionalidades da aplicação.
+
+O módulo cliente deve:
+● Ao iniciar, deve de inicializar o socket UDP através do qual enviará as mensagens
+UDP para o módulo servidor de ligações do usuário com o qual se deseja realizar a
+ligação.
+● O módulo cliente usa sua funcionalidade do cliente de registro para consultar o
+endereço IP do outro usuário. Para isso, deve usar a facilidade “consulta” do módulo
+cliente de registro especificando o destinatário da ligação.
+● Quando o cliente recebe a resposta com o endereço IP destino, é estabelecida uma
+chamada UDP com o par da ligação e é enviado uma mensagem de “convite”. Essa chamada
+UDP é realizada para o módulo servidor de ligação que está aguardando a chegada de
+mensagens UDP. Na mensagem “convite” é enviado o nome de usuário de quem está iniciando
+a ligação, além de seu próprio endereço IP e a porta usada para receber as mensagens UDP.
+● Quando recebe uma “resposta_ao_convite”, caso seja “rejeitado”, imprimir na tela a
+mensagem “usuário destino ocupado”, caso seja “ aceito”, inicia a coleta e transmissão
+Do sinal de áudio pela porta UDP.
+● Quando um usuário deseja encerrar a ligação envia uma mensagem “encerrar_ligação”
+para o servidor de ligação e para de transmitir áudio.
+● Quando recebe a mensagem “encerrar_ligação”, para a transmissão de áudio.
+"""
+###DEPOIS ESSE ARQUIVO DEVE SER COMBINADO AO ARQUIVO CLIENT.JS!!!!!
+
+################# CÓDIGO DO ARQUIVO CLIENTE.PY - ETAPA 1 #####################
+from socket import *
+import protocolo_registro
+
+ip_do_servidor = input("Digite o endereço IP do servidor: ")
+porta_do_servidor = 5000
+
+def enviar_mensagem_e_aguardar_resposta(mensagem):
+    socket_cliente = socket(AF_INET, SOCK_STREAM)
+    socket_cliente.connect((ip_do_servidor, porta_do_servidor))
+    socket_cliente.send(mensagem.encode())
+    resposta = socket_cliente.recv(1024).decode()
+    socket_cliente.close()
+    return resposta
+
+def enviar_mensagem(mensagem):
+    socket_cliente = socket(AF_INET, SOCK_STREAM)
+    socket_cliente.connect((ip_do_servidor, porta_do_servidor))
+    socket_cliente.send(mensagem.encode())
+    socket_cliente.close()
+
+def imprimir_mensagens(mensagem, resposta):
+    print("Cliente: \"{0}\"".format(mensagem))
+    if(resposta != ""): print("Servidor: \"{0}\"".format(resposta))
+
+def registrar_usuario(nome, ip, porta):
+    mensagem = protocolo_registro.get_mensagem_do_tipo_registrar("{0}, {1}, {2}".format(nome, ip, porta))
+    return mensagem, enviar_mensagem_e_aguardar_resposta(mensagem)
+    
+def consultar_usuario(nome):
+    mensagem = protocolo_registro.get_mensagem_do_tipo_consultar(nome)
+    return mensagem, enviar_mensagem_e_aguardar_resposta(mensagem)
+
+def fechar_conexao_com_o_servidor(nome):
+    mensagem = protocolo_registro.get_mensagem_do_tipo_fechar_conexao(nome)
+    enviar_mensagem(mensagem)
+    return mensagem
+
+usuario_registrado = False
+nome_do_usuario = ""
+ip_do_usuario = input("Digite seu ip: ")
+porta_do_usuario = input("Digite sua porta: ")
+while not usuario_registrado:
+    nome_do_usuario = input("Digite seu nome: ")    
+    mensagem, resposta = registrar_usuario(nome_do_usuario, ip_do_usuario, porta_do_usuario)
+    if(protocolo_registro.get_tipo_de_mensagem(resposta) == protocolo_registro.MENSAGEM_TIPO_OK):
+        usuario_registrado = True
+    imprimir_mensagens(mensagem, resposta)
+
+#################### FIM CÓDIGO DO ARQUIVO CLIENTE.PY - ETAPA 1 ############################
+import protocolo_ligacao
+
+IP_SERVIDOR_LIGACOES = 'localhost'
+PORTA_SERVIDOR_LIGACOES = 6000
+socket_cliente = socket(AF_INET, SOCK_DGRAM)
+
+def enviar_encerramento_ligacao(): pass
+def enviar_audio(audio): pass
+def gravar_audio(): pass
+def reproduzir_audio(): pass
+def pedido_encerramento_ligacao(info): pass
+def receber_informacoes(): pass
+
+def realizar_ligacao():
+    while True:
+        info = receber_informacoes()
+        if(pedido_encerramento_ligacao(info)): return
+        reproduzir_audio(info)
+        audio = gravar_audio()
+        enviar_audio(audio)
+        encerrar_ligacao = input("Deseja encerrar a ligação? ")
+        if(encerrar_ligacao):
+            enviar_encerramento_ligacao()
+            return
+
+def get_resposta_convite():
+    resposta, endereco = socket_cliente.recvfrom(1024)
+    return resposta
+
+def enviar_convite(nome, ip, porta, ip_dest, porta_dest):
+    convite = protocolo_ligacao.get_mensagem_do_tipo_convite("{0}, {1}, {2}".format(nome, ip, porta))
+    socket_cliente.sendto(convite.encode(), (ip_dest, porta_dest))
+
+while True:
+    nome_dest_ligacao= input("Para quem você deseja ligar? (digite \"quit\" para sair): ")
+    if(nome_dest_ligacao == "quit"):
+        mensagem = fechar_conexao_com_o_servidor(nome_do_usuario)
+        imprimir_mensagens(mensagem, "")
+        break
+    mensagem, resposta = consultar_usuario(nome_dest_ligacao)
+    imprimir_mensagens(mensagem, resposta)
+
+    if(protocolo_registro.get_tipo_de_mensagem(resposta) == protocolo_registro.MENSAGEM_TIPO_ERRO): continue
+    informacoes = protocolo_registro.get_informacoes_da_mensagem(resposta)
+    ip_dest, porta_dest = informacoes.split(", ")
+    porta_dest = int(porta_dest)
+    enviar_convite(nome_do_usuario, ip_do_usuario, porta_do_usuario, ip_dest, porta_dest)
+    resposta = get_resposta_convite()
+    info = protocolo_ligacao.get_informacoes_da_mensagem(resposta)
+    if(info == "Rejeitado"):
+        print("Usuário destino ocupado!")
+        continue
+    elif(info == "Aceito"):
+        realizar_ligacao()
+    
+
+
+"""
+IF RECEBE PEDIDO DE TERMINO() TERMINA LIGACAO
+IF QUER TERMINAR() TERMINA LIGACAO
+
+
+
+
+sentence = raw_input('Input lowercase sentence:')
+
+# Envio de bytes. Repare que o endereco do destinatario eh necessario
+clientSocket.sendto(sentence, (serverName, serverPort))
+
+# Recepcao
+modifiedSentence, addr = clientSocket.recvfrom(1024)
+print 'From Server {}: {}'.format(addr, modifiedSentence)
+
+# Fechamento
+clientSocket.close()
+"""
---
+++ TEMP/servidor_registro.py
@@ -0,0 +1,58 @@
+from socket import *
+from usuario import Usuario
+import protocolo_registro
+
+gUsuarios = {}
+
+def adicionar_novo_usuario(nome, ip, porta):
+    gUsuarios[nome] = Usuario(nome, ip, porta)
+
+def imprimir_usuarios():
+    print("\n########## Lista de Usuários ##########")
+    for usuario in gUsuarios.values():  usuario.imprimir()
+    print("#######################################\n")
+
+def imprimir_mensagens(mensagem, resposta):
+    print("Cliente: \"{0}\"".format(mensagem))
+    if(resposta != ""): print("Servidor: \"{0}\"".format(resposta))
+
+def processar_requisicao_de_registro(informacoes_da_mensagem):
+    nome, ip, porta = informacoes_da_mensagem.split(", ")
+    if(nome in gUsuarios.keys()):
+        return protocolo_registro.get_mensagem_do_tipo_erro("Já há um usuário registrado com esse nome!")
+    adicionar_novo_usuario(nome, ip, porta)
+    imprimir_usuarios()
+    return protocolo_registro.get_mensagem_do_tipo_ok("Novo usuário registrado!")
+
+def processar_requisicao_de_consulta(nome):
+    try:
+        usuario = gUsuarios[nome]
+        return protocolo_registro.get_mensagem_do_tipo_ok("{0}, {1}".format(usuario.ip, usuario.porta))
+    except:
+        return protocolo_registro.get_mensagem_do_tipo_erro("Não há nenhum usuário registrado com esse nome!")
+
+def processar_requisicao_de_fechamento_de_conexao(nome):
+    try:
+        gUsuarios.pop(nome)
+    except:
+        return
+
+porta_do_servidor = 5000
+socket_do_servidor = socket(AF_INET, SOCK_STREAM)
+socket_do_servidor.bind(('', porta_do_servidor))
+socket_do_servidor.listen(1)
+while 1:
+    socket_de_conexao, endereco = socket_do_servidor.accept()
+    mensagem = socket_de_conexao.recv(1024).decode()
+    tipo_de_mensagem = protocolo_registro.get_tipo_de_mensagem(mensagem)
+    informacoes_da_mensagem = protocolo_registro.get_informacoes_da_mensagem(mensagem)
+    resposta = ""
+    if(tipo_de_mensagem == protocolo_registro.MENSAGEM_TIPO_REGISTRAR):
+        resposta = processar_requisicao_de_registro(informacoes_da_mensagem)
+    elif(tipo_de_mensagem == protocolo_registro.MENSAGEM_TIPO_CONSULTAR):
+        resposta = processar_requisicao_de_consulta(informacoes_da_mensagem)
+    elif(tipo_de_mensagem == protocolo_registro.MENSAGEM_TIPO_FECHAR_CONEXAO):
+        processar_requisicao_de_fechamento_de_conexao(informacoes_da_mensagem)
+    imprimir_mensagens(mensagem, resposta)
+    if(resposta != ""): socket_de_conexao.send(resposta.encode())
+    socket_de_conexao.close()

bccf6ac45aaf5d7f4815f4aff02b771e86fede5f
JoaoLopez <joaolopez@id.uff.br>
2021-09-03 15:10:12 -0300

    Atualizaçao do protocolo de ligaçao. Inclusao de mensagem para envio de dados de voz

--- TEMP/protocolo_ligacao.py
+++ TEMP/protocolo_ligacao.py
@@ -3,19 +3,25 @@ Convidar um usuário:
     Cliente: "Convite: nome_cliente, ip_cliente, porta_cliente"
     Servidor: "Resposta ao Convite: Aceito"
     Servidor: "Resposta ao Convite: Rejeitado"
+Enviar dados de voz:
+    Cliente: "Dados: dados"
 Encerrar uma ligação:
     Cliente: "Encerrar Ligação:"
 """
 
 MENSAGEM_TIPO_CONVITE = 1
 MENSAGEM_TIPO_RESPOSTA_CONVITE = 2
-MENSAGEM_TIPO_ENCERRAR_LIGACAO = 3
+MENSAGEM_TIPO_DADOS = 3
+MENSAGEM_TIPO_ENCERRAR_LIGACAO = 4
 
 def get_mensagem_do_tipo_convite(informacoes):
     return "Convite: {0}".format(informacoes)
 
 def get_mensagem_do_tipo_resposta_convite(informacoes):
     return "Resposta ao Convite: {0}".format(informacoes)
+
+def get_mensagem_do_tipo_dados(informacoes):
+    return "Dados: {0}".format(informacoes)
 
 def get_mensagem_do_tipo_encerrar_ligacao():
     return "Encerrar Ligação:"
@@ -26,4 +32,5 @@ def get_informacoes_da_mensagem(mensagem):
 def get_tipo_de_mensagem(mensagem):
     if(mensagem.startswith("Convite")): return MENSAGEM_TIPO_CONVITE
     if(mensagem.startswith("Resposta ao Convite")): return MENSAGEM_TIPO_RESPOSTA_CONVITE
+    if(mensagem.startswith("Dados")): return MENSAGEM_TIPO_DADOS
     if(mensagem.startswith("Encerrar Ligação")): return MENSAGEM_TIPO_ENCERRAR_LIGACAO
